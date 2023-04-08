ccc88a41ad844803724b4171b318b02dd4a44f79
em1nm <em.martirosyan@gmail.com>
2023-04-07 19:02:50 -0700

    added functions for game, scoreboard and high scores

--- assets/script.js
+++ assets/script.js
@@ -15,9 +15,11 @@ let timerCount = 45;
 let buttons = [];
 let finalScore;
 let highscoreButton = document.getElementById("highscore");
+
+//questions and answers for quiz
 let questions = [
     {
-        question: "What was Kobe Bryant's high school jersey number?:",
+        question: "What was Kobe Bryant's high school jersey number?",
         answers: ["8", "24", "33", "34"],
         correctAnswer: "33"
     },
@@ -37,7 +39,52 @@ let questions = [
         correctAnswer: "Black Mamba"
     },
     {
-        question: "What prestigious award did Kobe Bryant win in 2018 for his contributions to the sport of basketball?:",
-        answers: ["NBA MVP", "NBA Finals MVP", "Olympic Gold Medal", "Academy Award"],
+        question: "What prestigious award did Kobe Bryant win in 2018 for his contributions to the sport of basketball?",
+        answers: ["Academy Award", "NBA Finals MVP", "Olympic Gold Medal", "NBA MVP"],
         correctAnswer: "Academy Award"
-    }]
+    }];
+
+    // shows score, timer and instructions including a start-quiz button on load page
+    onLoad();
+    function onLoad() {
+        score = 0;
+        timerCount = 45;
+        questionEl.remove();
+        container.appendChild(instructionText);
+        startButton.textContent = "Start Quiz"; 
+        container.appendChild(startButton); 
+    }
+
+    // function to display scoreboard
+    function displayScores() {
+        let scores = JSON.parse(localStorage.getItem("scores")) || [];
+        scores.sort(function (a, b) {
+            return b.score - a.score;
+        });
+        let scoreBoard = document.createElement("ul");
+        for (let i = 0; i < scores.length; i++) {
+            let userScore = document.createElement("li");
+            userScore.textContent = scores[i].initials + " highest score:" + scores[i].score;
+            scoreBoard.appendChild(userScore);
+        }}
+
+        container.appendChild(scoreBoard);
+    let backButton = document.createElement("button");
+    backButton.textContent = "Previous";
+    scoreBoard.appendChild(backButton);
+    backButton.addEventListener("click", function () {
+        onLoad();
+        scoreBoard.remove();
+        backButton.remove();
+        container.appendChild(highscoreButton);
+    });   
+
+    // function to display high scores:
+    highscoreButton.addEventListener("click", function () {
+        displayScores();
+        highscoreButton.remove();
+        questionEl.remove();
+        instructionText.remove();
+        hideAll();
+    });
+
